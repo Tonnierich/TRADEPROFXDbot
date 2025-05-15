896ce583c35cded845d191b48058712a46d3fbe5
Tradeprofx <tradeprofx9@gmail.com>
2025-05-15 17:40:12 +0300

    Update strategies.tsx

--- src/pages/strategies/strategies.tsx
+++ src/pages/strategies/strategies.tsx
@@ -34,6 +34,7 @@ const Strategies = observer(() => {
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
               allowFullScreen
+              style={{ display: "block", width: "100%", height: "100%" }}
             ></iframe>
           </div>
         </div>
